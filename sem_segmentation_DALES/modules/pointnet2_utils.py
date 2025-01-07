def4417a90d0b930c0be0e80bc8e39a9fba9afb2
Dejun Zhang <36147427+djzgroup@users.noreply.github.com>
2025-01-07 18:31:22 +0800

    Add files via upload

---
+++ sem_segmentation_DALES/modules/pointnet2_utils.py
@@ -0,0 +1,99 @@
+"""
+Author: Haoxi Ran
+Date: 05/10/2022
+"""
+
+import torch
+from pointnet2_ops import pointnet2_utils
+
+
+
+def square_distance(src, dst):
+    """
+    Calculate Squared distance between each two points.
+
+    """
+    B, N, _ = src.shape
+    _, M, _ = dst.shape
+    dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
+    dist += torch.sum(src ** 2, -1).view(B, N, 1)
+    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    return dist
+
+
+def index_points(points, idx):
+
+    device = points.device
+    B = points.shape[0]
+    view_shape = list(idx.shape)
+    view_shape[1:] = [1] * (len(view_shape) - 1)
+    repeat_shape = list(idx.shape)
+    repeat_shape[0] = 1
+    batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+    new_points = points[batch_indices, idx, :]
+    return new_points
+
+
+def farthest_point_sample(xyz, npoint, cuda=False):
+    """
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint]
+
+    FLOPs:
+        S * (3 + 3 + 2)
+    """
+
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = torch.max(distance, -1)[1]
+    return centroids
+
+
+def query_ball_point(radius, nsample, xyz, new_xyz):
+
+    device = xyz.device
+    B, N, C = xyz.shape
+    _, S, _ = new_xyz.shape
+    group_idx = torch.arange(N, dtype=torch.long).to(device).view(1, 1, N).repeat([B, S, 1])
+    sqrdists = square_distance(new_xyz, xyz)
+    group_idx[sqrdists > radius ** 2] = N
+    group_idx = group_idx.sort(dim=-1)[0][:, :, :nsample]
+    group_first = group_idx[:, :, 0].view(B, S, 1).repeat([1, 1, nsample])
+    mask = group_idx == N
+    group_idx[mask] = group_first[mask]
+
+    return group_idx
+
+
+def query_knn_point(k, xyz, new_xyz):
+
+
+    dist = square_distance(new_xyz, xyz)
+    group_idx = dist.sort(descending=False, dim=-1)[1][:, :, :k]
+    return group_idx
+
+
+def sample(nsample, feature):
+    feature = feature.permute(0, 2, 1)
+    xyz = feature[:, :, :3]
+
+    fps_idx = farthest_point_sample(xyz, nsample)  # [B, npoint, C]
+    torch.cuda.empty_cache()
+    feature = index_points(feature, fps_idx)
+    torch.cuda.empty_cache()
+    feature = feature.permute(0, 2, 1)
+
+    return feature
